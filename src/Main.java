8face1c432dcbda8f3fd3bf7979a9b64981a17f3
user <alexust11@yandex.ru>
2022-08-31 17:28:43 +0700

    заполнил массив работников

--- src/Main.java
+++ src/Main.java
@@ -8,9 +8,18 @@ public class Main {
         Employee [] employee=new  Employee[10];
         employee[0] = new Employee("Иванов", "Иван", "Иванович", 2, 50000);
         employee[1] = new Employee("Петров", "Петр", "Петрович", 2, 60000);
-        employee[2] = new Employee("Петров", "Петр", "Петрович", 2, 60000);
-        employee[3] = new Employee("Петров", "Петр", "Петрович", 2, 60000);
-        employee[4] = new Employee("Петров", "Петр", "Петрович", 2, 60000);
+        employee[2] = new Employee("Сидоров", "Николай", "Кузьмич", 1, 45000);
+        employee[3] = new Employee("Криворучко ", "Семен", "Семеныч", 1, 70000);
+        employee[4] = new Employee("Нилов", "Иннокентий", "Петрович", 3, 30000);
+        employee[5] = new Employee("Бобков", "Александр", "Степанович", 4, 43000);
+        employee[6] = new Employee("Баулин", "Николай", "Васильевч", 3, 54000);
+        employee[7] = new Employee("Раков", "Александр", "Юрьевич", 1, 66000);
+        employee[8] = new Employee("Шишкин", "Андрей", "Ильич", 2, 74000);
+        employee[9] = new Employee("Пономарев", "Игорь", "Анатольевич", 4, 48000);
+
+
+
+
 
         for (int i = 0; i < employee.length; i++)
             if (employee[i] != null) {
