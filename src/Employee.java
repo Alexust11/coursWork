4cfc67f628662e7af2ab03a830fda27eae7c55c6
user <alexust11@yandex.ru>
2022-08-31 17:23:07 +0700

    разобрался с идентификатором

--- src/Employee.java
+++ src/Employee.java
@@ -4,15 +4,72 @@ public class Employee {
     private String patronymic; //отчество
     private int department; // отдел
     private int wages;// заработная плата
-    private int id;
 
+    public static int counter=0;
 
-    public Employee(String surName, String name, String patronymic, int department, int wages, int id ) {
+    private synchronized static int nextCounter() {
+        return counter++;
+    }
+    public final int id;
+
+    {
+        id = nextCounter();
+    }
+
+    public Employee(String surName, String name, String patronymic, int department, int wages) {
         this.surName = surName;
         this.name = name;
         this.patronymic = patronymic;
         this.department = department;
         this.wages = wages;
-        this.id=id++;
+
     }
+
+    public String getSurName() {
+        return surName;
+    }
+
+    public void setSurName(String surName) {
+        this.surName = surName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
+
+    public int getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+
+    public int getWages() {
+        return wages;
+    }
+
+    public void setWages(int wages) {
+        this.wages = wages;
+    }
+
+    public  int getId() {
+        return id;
+    }
+
+
+
 }
+
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,22 @@
 import java.util.Arrays;
 
 public class Main {
-    public static int id;
+
     public static void main(String[] args) {
 
 
         Employee [] employee=new  Employee[10];
-        employee[1] = new Employee("Иванов", "Иван", "Иванович", 2, 50000);
+        employee[0] = new Employee("Иванов", "Иван", "Иванович", 2, 50000);
+        employee[1] = new Employee("Петров", "Петр", "Петрович", 2, 60000);
+        employee[2] = new Employee("Петров", "Петр", "Петрович", 2, 60000);
+        employee[3] = new Employee("Петров", "Петр", "Петрович", 2, 60000);
+        employee[4] = new Employee("Петров", "Петр", "Петрович", 2, 60000);
+
+        for (int i = 0; i < employee.length; i++)
+            if (employee[i] != null) {
+                System.out.println(employee[i].getId()) ;
+            }
+
 
 
     }
